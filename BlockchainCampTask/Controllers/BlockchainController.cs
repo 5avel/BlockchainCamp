46a1968f82011501be6e93b2083dcba1bf164697
Bolubash Pavel <pavel.b@live.ru>
2018-01-19 18:19:24 +0200

    fc

---
+++ BlockchainCampTask/Controllers/BlockchainController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlockchainCampTask.Controllers
+{
+    [Route("api")]
+    public class BlockchainController : Controller
+    {
+
+        // GET api/last_blocks/5
+        [HttpGet("last_blocks/{count}")]
+        public string Get(int count)
+        {
+            return "value";
+        }
+
+        // POST api/add_data/data:'{data}'
+        [HttpPost("add_data/data:{data}")]
+        public string Post(string data)
+        {
+            return data;
+        }
+
+        // POST api/add_data
+        [HttpPost("add_data")]
+        public string PostBody([FromBody]Data data)
+        {
+            return data.Value;
+        }
+
+    }
+
+    public struct Data
+    {
+        public string Value { get; set; }
+
+    }
+}
---
+++ BlockchainCampTask/Models/Block.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockchainCampTask.Models
+{
+    public class Block
+    {
+        public Block(string previous_block_hash, string[] rows, int timestamp)
+        {
+            this.previous_block_hash = previous_block_hash;
+            if (rows.Length != 5) throw new ArgumentException();
+            this.rows = rows;
+            this.timestamp = timestamp;
+            string str = Newtonsoft.Json.JsonConvert.SerializeObject(this);
+            using (var sha256 = SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(str));
+                this.block_hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+
+        }
+        public string previous_block_hash { get; }
+        public string[] rows { get; }
+        //int unixTime = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+        public int timestamp { get; }
+        public string block_hash { get; }
+
+    }
+}
