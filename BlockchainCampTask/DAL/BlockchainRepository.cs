4cb841d1a4952663c1ee681e08bfcb9ef8623605
Bolubash Pavel <pavel.b@live.ru>
2018-02-01 07:42:59 +0200

    sync2

---
+++ BlockchainCampTask/DAL/BlockchainRepository.cs
@@ -0,0 +1,39 @@
+﻿using BlockchainCampTask.Models;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockchainNode.DAL
+{
+    public class BlockchainRepository
+    {
+        // get Blocks (0  = all blocks, count < num - all blocks)
+        public List<Block> GetBlocks(int count)
+        {
+            List<Block> blocksToReturn = new List<Block>();
+            using (var db = new LiteDatabase(@"Data/Blockchain.db"))
+            {
+                // Get customer collection
+                var blocks = db.GetCollection<Block>("blocks");
+                var results = blocks.FindAll();
+                foreach (Block block in results)
+                {
+                    blocksToReturn.Add(block);
+                }
+                return blocksToReturn;
+            }
+        }
+        // add new block
+
+        // get all links
+        // add link
+
+        // get all Transactions
+        // add Transaction
+
+        // GetStatus
+        // save status
+    }
+}
--- BlockchainCampTask/Models/Link.cs
+++ BlockchainCampTask/Models/Link.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BlockchainCampTask.Models
+﻿namespace BlockchainCampTask.Models
 {
     public class Link
     {
-      
         public string id { get; set; }
         public string url { get; set; }
     }
