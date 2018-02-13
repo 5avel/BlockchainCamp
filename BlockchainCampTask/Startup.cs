580a19279ab888fee884a53d6f4b6a2711b9608b
Bolubash Pavel <pavel.b@live.ru>
2018-02-13 17:25:17 +0200

    ConsoleLoggs

--- BlockchainCampTask/Startup.cs
+++ BlockchainCampTask/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,12 +30,36 @@ namespace BlockchainCampTask
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+           
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(async (context, next) =>
+            {
+                using (var bodyReader = new StreamReader(context.Request.Body))
+                {
+                    string body = await bodyReader.ReadToEndAsync();
+
+                    context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(body));
+
+                    
+                    Console.WriteLine("{0} Processing request {1}",DateTime.Now.ToString(), context.Request.Path);
+                    if(!String.IsNullOrWhiteSpace(context.Request.QueryString.Value))
+                        Console.WriteLine("Processing QueryString {0}", context.Request.QueryString.Value);
+                    if (!String.IsNullOrWhiteSpace(body))
+                    {
+                        System.Diagnostics.Debug.Print(body);
+                        Console.WriteLine("Processing Body {0}", body);
+                    }
+                }
+               
+                await next.Invoke();
+            });
 
             app.UseMvc();
         }
